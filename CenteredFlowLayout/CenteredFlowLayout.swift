63068a170b0253ae666a0e3edc946b737aaff2b2
Nilson Souto <xissburg@Nilsons-MacBook-Air.local>
2016-06-15 12:38:35 +0100

    (っ˘▽˘)っ ☁️☁️☁️ ⊂(◕。◕⊂)

---
+++ CenteredFlowLayout/CenteredFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  CenteredFlowLayout.swift
+//  CenteredFlowLayout
+//
+//  Created by Nilson Souto on 6/15/16.
+//  Copyright © 2016 xissburg. All rights reserved.
+//
+
+import UIKit
+
+class CenteredFlowLayout: UICollectionViewLayout {
+
+}
